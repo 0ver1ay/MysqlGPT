d5ccff15e17d073e05682247e137ba6c60485b13
0ver1ay <ovelay@gmail.com>
2024-12-17 19:03:48 +0300

    commit 2

---
+++ PHPmyadmin/tmp/twig/46/46605c6dfe6808984b623d8e38e8c5d5.php
@@ -0,0 +1,62 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* display/results/empty_display.twig */
+class __TwigTemplate_d180a9e7f21f62fa0424ccddd1f73312 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<td class=\"";
+        echo twig_escape_filter($this->env, ($context["classes"] ?? null), "html", null, true);
+        echo "\"></td>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "display/results/empty_display.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "display/results/empty_display.twig", "E:\\WORK\\Python\\mysql_GPT_fill\\PHPmyadmin\\templates\\display\\results\\empty_display.twig");
+    }
+}
